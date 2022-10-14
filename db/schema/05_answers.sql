27ebcb0f2dbb72f20e2dcab32f4e49b404402ff1
Megan Honey <mjthoney@gmail.com>
2022-10-14 00:11:19 +0000

    Add on delete cascade to answers table

--- db/schema/05_answers.sql
+++ db/schema/05_answers.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS answers CASCADE;
 
 CREATE TABLE answers (
   id SERIAL PRIMARY KEY NOT NULL,
-  question_id INTEGER REFERENCES questions(id),
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
   answer TEXT NOT NULL
 );
