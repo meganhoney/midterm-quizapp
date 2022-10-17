ef5a1fa6131aad622d62a9344f5ef5892a21e892
Megan Honey <mjthoney@gmail.com>
2022-10-17 22:17:58 +0000

    initial index file with display all quizzes function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,34 @@
-// Client facing scripts here
+$(document).ready(function() {
+  console.log('data');
+
+  const loadQuizzes = function() {
+    $.ajax("/api/quizzes", {
+      method: "GET"
+    })
+      .then((response) => {
+        displayQuizzes(response);
+      });
+  };
+
+  loadQuizzes();
+
+  const createQuizElement = function(quiz) {
+    // html markup
+    let $quiz = `
+      <article>
+      <table>
+        <p>${quiz.title}</p>
+      </article>
+    `;
+    return $quiz;
+  };
+
+  const displayQuizzes = function(quizzes) {
+
+    const $quizzes = $("#quizzes").empty();
+    console.log(quizzes);
+    for (let key of Object.keys(quizzes)) {
+      $(createQuizElement(quizzes[key])).appendTo($quizzes);
+    }
+  };
+});
