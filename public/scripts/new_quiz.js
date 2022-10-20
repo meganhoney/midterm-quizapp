0fc2c4487cfbca3e80fd4b4b6127f67b75998a6e
Megan Honey <mjthoney@gmail.com>
2022-10-20 03:57:05 +0000

    Add functionality for inputting fill in the blank questions

--- public/scripts/new_quiz.js
+++ public/scripts/new_quiz.js
@@ -4,13 +4,23 @@ $(document).ready(function() {
     const id = this.id.split("-").pop();
     const $val = $(this).val();
     const $input = $(`#question-input-${id}`);
-
     if($val === "1") {
+      $input.empty();
       $(generateMultipleChoiceInput(id)).appendTo($input);
     } else if($val === "2") {
-      $(generateFillBlankInput).appendTo($input);
+      $input.empty();
+      $(generateFillInTheBlankInput(id)).appendTo($input);
+      $(".insert-blank").click(function() {
+        const $id = $(this).prev().attr("id");
+        const $textToEdit = ($(this).prev().val());
+        const $blank = "_____ ";
+        const $cursorPosition = document.getElementById(`${$id}`).selectionStart;
+        $(`#${$id}`).val($textToEdit.slice(0, $cursorPosition)+$blank+$textToEdit.slice($cursorPosition));
+      });
     } else if($val === "Question Type") {
       $input.empty();
     }
-  })
+  });
+
+
 });
--- public/scripts/new_quiz_helpers.js
+++ public/scripts/new_quiz_helpers.js
@@ -2,7 +2,7 @@
 const generateMultipleChoiceInput = function(id) {
   const $multipleChoice = `
   <label for="quiz-question-text" class="form-label">Question:</label>
-  <input type="text" class="form-control mb-2" id="quiz-question-text-${id}" placeholder="Please type your question">
+  <input type="text" class="form-control mb-2" id="q${id}-text" placeholder="Please type your question">
   <div class="d=flex">
     <div class="input-group mb-3">
       <fieldset class="d-flex">
@@ -34,9 +34,17 @@ const generateMultipleChoiceInput = function(id) {
   return $multipleChoice;
 };
 
-const generateFillBlankInput = function() {
+const generateFillInTheBlankInput = function(id) {
   const $fillInTheBlank = `
-  <p>TEST</p>
+  <div class="d-flex mb-3">
+          <label for="qID-text" class="form-label">Question:</label>
+          <input type="text" class="form-control ml-2" id="q${id}-text" placeholder="Type question text here then use button to insert blank space">
+          <button type="button" class="btn btn-sm btn-outline-dark text-nowrap insert-blank">Insert blank space</button>
+        </div>
+        <div class="d-flex mb-3">
+          <label for="q${id}-answer" class="form-label">Answer:</label>
+          <input type="text" class="form-control ml-2" id="QUESTIONB" placeholder="Type the word that fills in the blank">
+      </div>
   `;
 
   return $fillInTheBlank;
