1fc5afaca6b53cc2d9c52341ab192fcb2a3447dd
Gene Tenorlas <gene.tenorlas@yahoo.com>
2022-10-21 13:36:05 +0000

    Create /scripts/quiz_all_results

---
+++ .history/public/scripts/quiz_all_results_20221021080537.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+  let getId = document.location.href.split("/");
+  let id = getId.pop();
+
+  const loadResult = function () {
+
+    $.ajax(`api/my_quizzes/${id}`, {
+      method: "GET"
+    })
+      .then((response) => {
+        displayResult(response);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
+  loadResult();
+
+  const displayResult = (quizResults) => {
+    const $sectionDetails = $('.details');
+    const $sectionShare = $('.share');
+    const text = `
+    <h1>
+    ${quizResults.title}
+    </h1>
+
+    `;
+
+    $sectionDetails.append(text);
+
+    const shareText = `
+    <!-- The text field -->
+    <input type="text" value='http://localhost:8080/my_quizzes/${id}' id="myInput">
+
+    <!-- The button used to copy the text -->
+    <button id="share_button">Share URL</button>
+    `;
+
+    $sectionShare.append(shareText);
+
+    $("#share_button").click(() => {
+      const $temp = $("#myInput");
+      $temp.select();
+      document.execCommand("copy");
+      alert(`Copied URL: http://localhost:8080/attempted/${id}
+      `);
+    });
+
+  }
+
+});
